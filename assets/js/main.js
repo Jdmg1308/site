08b38c242de84874e39494bb74ba9c790dba4239
Jdmg1308 <juandiegomendezgutierrez@gmail.com>
2024-12-25 17:21:07 -0500

    Almost done

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,6 +286,58 @@
 
 			};
 
+		// Image Track
+
+		const track = document.getElementById("image-track");
+		let isMouseDown = false;
+
+		track.addEventListener("mousedown", (e) => {
+			isMouseDown = true;
+			track.dataset.mouseDownAt = e.clientX;
+		});
+
+		track.addEventListener("mouseup", () => {
+			isMouseDown = false;
+			track.dataset.mouseDownAt = "0";
+			track.dataset.prevPercentage = track.dataset.percentage;
+		});
+
+		track.addEventListener("mouseleave", () => {
+			isMouseDown = false;
+			track.dataset.mouseDownAt = "0";
+			track.dataset.prevPercentage = track.dataset.percentage;
+		});
+
+		track.addEventListener("mousemove", (e) => {
+			if (!isMouseDown) return;
+
+			const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+			const mouseMax = window.innerWidth / 5; // Allow a third of the screen for scrolling
+
+			const percentage = (mouseDelta / mouseMax) * -100;
+			const nextPercentage = Math.max(
+				Math.min(parseFloat(track.dataset.prevPercentage) + percentage, 0),
+				-800
+			);
+
+			track.dataset.percentage = nextPercentage;
+
+			// Smooth track movement
+			track.animate(
+				{ transform: `translate(${nextPercentage}%, 0%)` },
+				{ duration: 1300, fill: "forwards" }
+			);
+
+			// Smooth image position changes
+			for (const image of track.getElementsByClassName("image")) {
+				image.animate(
+					{ objectPosition: `${100 + nextPercentage / 5}% center` },
+					{ duration: 1200, fill: "forwards" }
+				);
+			}
+		});
+
+
 		// Articles.
 			$main_articles.each(function() {
 
